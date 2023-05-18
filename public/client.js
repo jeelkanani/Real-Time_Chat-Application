e8bdfb1bcea93cd8d84c3b7f9492f55da9e14029
jeelkanani <jeelkanani@gmail.com>
2023-05-18 10:39:03 +0530

    Install socket.io,setup socket.io in server.js and client.js

---
+++ public/client.js
@@ -0,0 +1,2 @@
+// in html file return client library so here you can use it
+const socket=io()
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const { Socket } = require('socket.io');
 const app=express();
 const PORT=process.env.PORT || 4000;
 
@@ -14,6 +15,16 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
+})
+
+
+//socket
+
+// here also menstion server name so socket know which server works
+const io = require('socket.io')(http);
+
+io.on('connection',(Socket)=>{
+    console.log('a user connected');
 })
 
 
