0b0275852648e10ebbaa494bfd5638b23fec089f
jeelkanani <jeelkanani@gmail.com>
2023-05-18 14:01:36 +0530

    implement logic to real time chat

--- public/client.js
+++ public/client.js
@@ -1,2 +1,71 @@
-// in html file return client library so here you can use it
-const socket=io()
+const socket = io()
+let name;
+let textarea = document.querySelector('textarea')
+let messageArea = document.querySelector('.message__area')
+let image=  document.getElementById('msgsend')
+
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+ 
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+
+})
+
+
+image.addEventListener('click', function(){
+    const text=textarea.value;
+    sendMessage(text)
+    
+})
+
+
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message
+    }
+    // Append 
+    if(msg.message !== '')
+    {
+     appendMessage(msg, 'outgoing')
+     textarea.value = ''
+     scrollToBottom()
+
+    // Send to server 
+    
+    socket.emit('message', msg)
+    }
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-const express=require('express');
-const { Socket } = require('socket.io');
-const app=express();
-const PORT=process.env.PORT || 4000;
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
 
-const http=require('http').createServer(app);
+const PORT = process.env.PORT || 3000
 
-
-
-http.listen(PORT, ()=>{
-    console.log(`listening on ${PORT}`);
-});
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
+// Socket 
+const io = require('socket.io')(http)
 
-//socket
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
 
-// here also menstion server name so socket know which server works
-const io = require('socket.io')(http);
-
-io.on('connection',(Socket)=>{
-    console.log('a user connected');
 })
-
-
